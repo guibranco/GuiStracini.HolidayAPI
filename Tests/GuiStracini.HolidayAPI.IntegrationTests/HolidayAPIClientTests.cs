ea60c6103b469f2b53a07be2e2fe2a160e8bbaa8
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-01-28 05:00:46 +0400

    Update docs

--- Tests/GuiStracini.HolidayAPI.IntegrationTests/HolidayAPIClientTests.cs
+++ Tests/GuiStracini.HolidayAPI.IntegrationTests/HolidayAPIClientTests.cs
@@ -4,7 +4,7 @@
 // Created          : 01-05-2022
 //
 // Last Modified By : Guilherme Branco Stracini
-// Last Modified On : 01-05-2022
+// Last Modified On : 28/01/2023
 // ***********************************************************************
 // <copyright file="HolidayAPIClientTests.cs" company="GuiStracini.HolidayAPI.Tests">
 //     Copyright (c) Guilherme Branco Stracini ME. All rights reserved.
